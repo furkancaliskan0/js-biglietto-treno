55a9e9fa918d3ce20b8f0abffa4e95998353b01f
furkanGit <furkancaliskan9801@gmail.com>
2022-11-07 17:52:40 +0100

    Page Completed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,53 @@
 // Il programma deve Calcolare il prezzo del biglietto per il treno
 
+// elenco variabili
+let age, km, ticketCost;
 
+let priceForKm = 0.21;
 
 // chiedere quanti chilometri vuole percorrere l'utente
 
-let km= parseInt(prompt("Quanti Km vuole percorrere?"));
+km= parseInt(prompt("Quanti Km vuole percorrere?"));
+console.log(km);
 
 // chiedere quanti anni ha l'utente
 
-let age= parseInt(prompt("Mi dica la sua età"));
+age= parseInt(prompt("Qual'è la sua età?"));
+console.log(age);
+
+// calcolare il prezzo totale del viaggio 
+
+//prezzo per il biglietto è 0.21 € al km
+
+    ticketCost= km * priceForKm;
+
+    console.log(ticketCost);
+
+    
+
+// se è minorenne verrà applicato lo sconto del 20%
+let discountYoung=  ticketCost * 20 / 100;
 
 
+//se è anziano verrà applicato lo sconto del 40%
+let discounOld =  ticketCost * 40 / 100;
+
+
+
+// Calcolare il prezzo del biglietto in base ai dati 
+
+if (age < 18){
+    let priceYoung = (ticketCost - discountYoung).toFixed(2);
+    
+    document.getElementById("title").innerHTML = `Il prezzo del tuo biglietto è di ${priceYoung}€ euro`;
+
+}else if( age > 65 ){
+    let priceOld = (ticketCost - discounOld).toFixed(2);
+
+    document.getElementById("title").innerHTML = `Il prezzo del tuo biglietto è di ${priceOld}€ euro`;
+}else{
+    ticketCost;
+
+    document.getElementById("title").innerHTML = `Il prezzo del tuo biglietto è di ${ticketCost}€ euro`;
+}
 
