aa7a19fe5fd5441177343e1d9d9a036afa92ff61
furkanGit <furkancaliskan9801@gmail.com>
2022-11-07 15:54:51 +0100

    js structured

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Il programma deve Calcolare il prezzo del biglietto per il treno
+
+
+
+// chiedere quanti chilometri vuole percorrere l'utente
+
+let km= parseInt(prompt("Quanti Km vuole percorrere?"));
+
+// chiedere quanti anni ha l'utente
+
+let age= parseInt(prompt("Mi dica la sua età"));
+
+
+
